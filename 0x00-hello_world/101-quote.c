5f9976730abe69534710c28ca2fdaf1be5ceb009
SHmohamed229 <SHmohamed229@gmail.com>
2023-03-16 14:25:14 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
-#include <unistd.h> 
+#include <unistd.h>
+
 /**
-*main - Entry point 
-*Return: always 1 (success)
+ * main - Entry point
+ *
+ * Description: print a qoute using write function
+ *    size_t write(int fd, const void *buf, size_t count);
+ *
+ * Return: 1 (error).
 */
+
+
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
 	return (1);
 }
