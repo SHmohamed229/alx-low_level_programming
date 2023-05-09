5ff08c0f12874ec13cdd68855beac2213a826bae
SHmohamed229 <SHmohamed229@gmail.com>
2023-05-08 19:50:05 -0700

    up1

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -64,7 +64,7 @@ int main(int argc, char *argv[])
 	buff = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buff, 1024);
-	to = open(argv[2], O_CREAT | O_TRUNC, 0664);
+	to = open(argv[2], O_CREAT | O_TRUNC | O_TRUNC, 0664);
 
 	do {
 		if (from == -1 || r == -1)
